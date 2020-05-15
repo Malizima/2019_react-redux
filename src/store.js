71c25a45770a14de6d7433124b906b2c4b5ed9b9
Kim james <dy2409@gmail.com>
2020-05-15 10:17:39 +0900

    2nd

---
+++ src/store.js
@@ -0,0 +1,61 @@
+import {createStore} from "redux";
+import {createAction, createReducer, configureStore} from "@reduxjs/toolkit";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+// const addToDo = text => {
+//     return{
+//         type: ADD,
+//         text
+//     };
+// };
+
+//  const deleteToDo = id => {
+//     return{
+//         type: DELETE,
+//         id : parseInt(id)
+//     };
+// };
+
+
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
+
+
+// const reducer =(state = [], action) => {
+//     switch (action.type) {
+//         // case ADD:
+//         case addToDo.type:
+//             // return [{text: action.text, id:Date.now()}, ...state];
+//             return [{text: action.payload, id:Date.now()}, ...state];
+//         // case DELETE:
+//         case deleteToDo.type:
+//             return  state.filter(toDo => toDo.id !== action.payload);
+//         default:
+//             return state;
+//     }
+// };
+
+const reducer = createReducer([], {
+    [addToDo]: (state, action) => {
+        state.push({text: action.payload, id:Date.now()});
+    },
+    [deleteToDo]: (state, action) => 
+        state.filter(toDo => toDo.id !== action.payload)
+})
+
+
+
+
+
+// const store = createStore(reducer);
+
+const store = configureStore({ reducer});
+
+export const actionCreators = {
+    addToDo,
+    deleteToDo
+}
+
+export default store;
